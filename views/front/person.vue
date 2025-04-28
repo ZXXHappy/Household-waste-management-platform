3809ba8b340ee9700115b12c10ed24e9760893a1
yzsssd <2024696048@qq.com>
2025-04-28 23:24:40 +0800

    用户信息编辑表单
    头像上传、基本信息展示与编辑功能，并通过表单实现用户数据的保存操作

---
+++ views/front/person.vue
@@ -0,0 +1,70 @@
+<template>
+  <div style="width: 40%; margin: 5px auto" class="card">
+    <el-form ref="user" :model="data.user" label-width="80px" style="padding: 20px">
+      <div style="text-align: center; margin-bottom: 20px">
+        <el-upload
+            :action="baseUrl + '/files/upload'"
+            :on-success="handleFileUpload"
+            :show-file-list="false"
+            class="avatar-uploader"
+        >
+          <img v-if="data.user.avatar" :src="data.user.avatar" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+        </el-upload>
+      </div>
+      <el-form-item prop="username" label="用户名">
+        <el-input disabled v-model="data.user.username" placeholder="请输入用户名"></el-input>
+      </el-form-item>
+      <el-form-item prop="name" label="姓名">
+        <el-input v-model="data.user.name" placeholder="请输入姓名"></el-input>
+      </el-form-item>
+      <el-form-item prop="phone" label="电话">
+        <el-input v-model="data.user.phone" placeholder="请输入电话"></el-input>
+      </el-form-item>
+      <el-form-item prop="email" label="邮箱">
+        <el-input v-model="data.user.email" placeholder="请输入邮箱"></el-input>
+      </el-form-item>
+      <el-form-item prop="score" label="剩余积分">
+        {{ data.user.score }}
+      </el-form-item>
+      <div style="text-align: center">
+        <el-button type="primary" @click="update">保 存</el-button>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.avatar-uploader {
+  height: 120px;
+}
+.avatar-uploader .avatar {
+  width: 120px;
+  height: 120px;
+  display: block;
+}
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 50%;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 120px;
+  height: 120px;
+  text-align: center;
+}
+</style>
