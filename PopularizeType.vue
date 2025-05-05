3ba1bf65424549c1a0e080d52d03918e1202a418
heheshui <169071759+heheshui@users.noreply.github.com>
2025-05-05 23:58:40 +0800

    垃圾类别及查询

---
+++ PopularizeType.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="card" style="margin-bottom: 5px">
+      <el-input v-model="data.name" prefix-icon="Search" style="width: 240px; margin-right: 10px" placeholder="请输入分类名称查询"></el-input>
+      <el-button type="info" plain @click="load">查询</el-button>
+      <el-button type="warning" plain style="margin: 0 10px" @click="reset">重置</el-button>
+    </div>
+    <div class="card" style="margin-bottom: 5px">
+      <el-button type="primary" plain @click="handleAdd">新增</el-button>
+      <el-button type="danger" plain @click="delBatch">批量删除</el-button>
+    </div>
+
+    <div class="card" style="margin-bottom: 5px">
+      <el-table stripe :data="data.tableData" @selection-change="handleSelectionChange">
+        <el-table-column type="selection" width="55" />
+        <el-table-column prop="name" label="分类名称" />
+        <el-table-column label="图片">
+          <template #default="scope">
+            <el-image style="width: 100px; height: 50px; border-radius: 5px" :src="scope.row.img" :preview-src-list="[scope.row.img]" preview-teleported></el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="100" fixed="right">
+          <template v-slot="scope">
+            <el-button type="primary" circle :icon="Edit" @click="handleEdit(scope.row)"></el-button>
+            <el-button type="danger" circle :icon="Delete" @click="del(scope.row.id)"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+    <div class="card" v-if="data.total">
+      <el-pagination @current-change="load" background layout="total, prev, pager, next" :page-size="data.pageSize" v-model:current-page="data.pageNum" :total="data.total" />
+    </div>
+
+    <el-dialog title="宣传分类信息" v-model="data.formVisible" width="40%" destroy-on-close>
+      <el-form ref="form" :model="data.form" label-width="70px" style="padding: 20px">
+        <el-form-item prop="name" label="分类名称">
+          <el-input v-model="data.form.name" placeholder="请输入分类名称"></el-input>
+        </el-form-item>
+        <el-form-item label="图片">
+          <el-upload
+              :action="baseUrl + '/files/upload'"
+              :headers="{ 'token': data.user.token }"
+              :on-success="handleFileUpload"
+              list-type="picture"
+          >
+            <el-button type="primary">上传</el-button>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="data.formVisible = false">取 消</el-button>
+          <el-button type="primary" @click="save">确 定</el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
