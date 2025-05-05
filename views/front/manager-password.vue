ae85de34699251f0add24245a0fe1ab89b6dac9f
yzsssd <2024696048@qq.com>
2025-05-05 10:21:02 +0800

    管理员用户密码修改表单
    包含原密码、新密码和确认密码的输入框，并提供密码可见切换功能，最后通过保存按钮提交修改。

---
+++ views/front/manager-password.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="width: 50%" class="card">
+    <el-form ref="formRef" :rules="data.rules" :model="data.user" label-width="80px" style="padding: 20px">
+      <el-form-item label="原密码" prop="password">
+        <el-input v-model="data.user.password" placeholder="请输入原密码" show-password></el-input>
+      </el-form-item>
+      <el-form-item label="新密码" prop="newPassword">
+        <el-input v-model="data.user.newPassword" placeholder="请输入新密码" show-password></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="confirmPassword">
+        <el-input v-model="data.user.confirmPassword" placeholder="请确认新密码" show-password></el-input>
+      </el-form-item>
+      <div style="text-align: center">
+        <el-button type="primary" @click="updatePassword">保 存</el-button>
+      </div>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
